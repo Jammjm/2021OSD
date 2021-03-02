678d8b2257fd174818064e4833b6692611686c48
HP_Omen <jammjmdev@gmail.com>
2021-03-02 19:02:50 +0900

    -1

---
+++ src/osd/Assign_0.java
@@ -0,0 +1,50 @@
+package osd;
+
+import java.util.Scanner;
+
+public class Assign_0 {
+	
+	static class Node{
+		public int degree;
+		public int coefficient;
+		public Node next;
+		public Node prev;
+		
+		public Node(int degree, int coefficient) {
+			this.degree = degree;
+			this.coefficient = coefficient;
+			this.next = null;
+			this.prev = null;
+		}
+	}
+	static class NodeList{
+		public Node first;
+		public Node last;
+		
+		public NodeList(){
+			
+		}
+		
+		
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		inputpoly();
+	}
+	
+	static Node inputpoly() {
+		Scanner sc = new Scanner(System.in);
+		while(true) {
+			System.out.print("Input (degree) (coefficient): ");
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			if(a<0 && b<0)
+				break;
+			
+		}
+		return null;
+	}
+
+}
